7d60a32bb8b76821fdafcfb15da2a248322d2726
AKASH THER <113231176+AKASHTHER@users.noreply.github.com>
2023-01-09 11:03:26 +0530

    Update flop_mul.v

--- flop_mul.v
+++ flop_mul.v
@@ -1,32 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/30/2022 10:56:38 AM
-// Design Name: 
-// Module Name: flop_mul
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 // 16 bit floating point multiplier
 
 module flop_mul(flp_a, flp_b, sign, exponent, prod);
 input [15:0] flp_a ;
 input [15:0] flp_b ;
 output reg sign ;
-//output [7:0] exponent  ;
 output [9:0] prod  ;
 reg [21:0] x;
 output reg [4:0] exponent;
@@ -35,8 +13,7 @@ always@ *
     begin
         if (flp_a==0||flp_b==0) 
             begin 
-               // exp_unbiased= flp_a[1:8]+flp_b[1:8]-254+1;
-               sign=0;
+              sign=0;
                x=0;
                exponent=0;
             end     
@@ -49,9 +26,7 @@ always@ *
                  begin
                      x = x <<1;
                      exponent = exponent - 1;
-                    //exp_unbiased=exp_unbiased-1;
-                 end 
-            // for(int i=0;i<32;i++)
+                    end 
            end
     end
 
